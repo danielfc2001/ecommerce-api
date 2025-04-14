83166594b0324d035506bb0abd90a23ea6b30a11
unknown <danielfundorc@gmail.com>
2025-04-13 22:01:11 -0400

    Fixing verifyCaptcha function

--- src/libs/verifyCaptcha.js
+++ src/libs/verifyCaptcha.js
@@ -1,18 +1,32 @@
 const SECRET_KEY = process.env.CAPTCHA_SECRET;
 
 export const verifyCaptcha = async (token) => {
-  const secretKey = SECRET_KEY;
-  const response = await fetch(
-    "https://challenges.cloudflare.com/turnstile/v0/siteverify",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: `secret=${secretKey}&response=${token}`,
-    }
-  );
+  if (!token) {
+    throw new Error("El token del CAPTCHA es inválido o está vacío.");
+  }
 
-  const data = await response.json();
-  return data.success;
+  try {
+    const response = await fetch(
+      "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `secret=${SECRET_KEY}&response=${token}`,
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        `Error en la solicitud: ${response.status} ${response.statusText}`
+      );
+    }
+
+    const data = await response.json();
+    return data.success;
+  } catch (error) {
+    console.error("Error al verificar el CAPTCHA:", error);
+    throw new Error("No se pudo verificar el CAPTCHA.");
+  }
 };
