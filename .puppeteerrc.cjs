0f719d0bb320c8dd4204d20ff81626ea65509d09
Daniel Fundora <danielfundorc@gmail.com>
2025-04-30 18:20:24 -0400

    Added puppeteer config file.

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,13 @@
+const { join } = require("path");
+
+module.exports = {
+  // Download Chrome (default `skipDownload: false`).
+  chrome: {
+    skipDownload: false,
+  },
+  // Download Firefox (default `skipDownload: true`).
+  firefox: {
+    skipDownload: true,
+  },
+  cacheDirectory: join(__dirname, "node_modules", ".cache", "puppeteer"),
+};
